bf8d0045a4537463ab42b6f61623ef27a4fa5ee6
Vishwashvar Prasanth T <vishwashvarprasanth@gmail.com>
2026-07-16 19:05:16 +0530

    Added LLM roadmap generation routes

--- backend/server.js
+++ backend/server.js
@@ -11,13 +11,15 @@ const app = express()
 app.use(cors()) // cross origin relational connection 
 app.use(express.json()) // it takes given input as json format 
 
-app.use('/api/auth', require('./routes/authRoutes'))
+app.use('/api/auth', require('./routes/authRoutes')) // if server comes this type of endpoint , access these folders, and this the rootpath
+
+app.use('/api/assessment', require('./routes/assessmentRoutes'))  // add this
+
+app.use('/api/roadmap', require('./routes/roadmapRoutes'))// add this
 
 app.get('/', (req, res) => {
   res.send('API is running')
 })
-
-app.use('/api/assessment', require('./routes/assessmentRoutes'))  // add this
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
